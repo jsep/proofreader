bdc1cc3c87a2348d614862b32c25b15b99d3964f
Juan Sepulveda <juan@easyappshq.com>
2025-01-16 07:58:47 -0400

    build: add vitest runner

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
